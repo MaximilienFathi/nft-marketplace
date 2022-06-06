b73638543adcb0e30b7c2f20beb9ba9bc53db229
Maximilien Fathi <maximilian.fathi@gmail.com>
2022-06-05 22:02:29 -0400

    Add smooth scrolling and sticky navigation

---
+++ index.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// ELEMENTS
+const allLinks = document.querySelectorAll("a:link");
+const heroSectionEl = document.querySelector(".hero-section");
+
+// SMOOTH SCROLLING ANIMATION
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+// STICKY NAVIGATION
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(entries);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-100%",
+  }
+);
+obs.observe(heroSectionEl);
