ead26109d644fd0a11391a5dc601898109203196
Maximilien Fathi <maximilian.fathi@gmail.com>
2022-06-07 23:52:13 -0400

    Add authentication form

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 "use strict";
 
+//////////////////////////////////////////////////
 // ELEMENTS
 const allLinks = document.querySelectorAll("a:link");
 const heroSectionEl = document.querySelector(".hero-section");
 const yearEl = document.querySelector(".year");
 
+const loginEl = document.querySelector(".login-btn");
+const signUpEl = document.querySelector(".sign-up-btn");
+const modalEl = document.querySelector(".modal-container");
+const modalCloseEl = document.querySelector(".modal-close");
+
+//////////////////////////////////////////////////
 // SMOOTH SCROLLING ANIMATION
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
@@ -26,6 +33,7 @@ allLinks.forEach(function (link) {
   });
 });
 
+//////////////////////////////////////////////////
 // STICKY NAVIGATION
 const obs = new IntersectionObserver(
   function (entries) {
@@ -48,6 +56,39 @@ const obs = new IntersectionObserver(
 );
 obs.observe(heroSectionEl);
 
+//////////////////////////////////////////////////
 // SET CURRENT YEAR
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear.toString();
+
+//////////////////////////////////////////////////
+// ENABLE MODAL FORM INTERACTION
+
+// When the user clicks on the LOGIN button, open the modal
+loginEl.onclick = function () {
+  modalEl.style.display = "flex";
+  document.querySelector("body").style.overflow = "hidden";
+  document.querySelector("#chk").checked = true;
+  // transition: 0.5s ease-in-out
+};
+
+// When the user clicks on the SIGN UP button, open the modal
+signUpEl.onclick = function () {
+  modalEl.style.display = "flex";
+  document.querySelector("body").style.overflow = "hidden";
+  document.querySelector("#chk").checked = false;
+};
+
+// When the user clicks on <span> (x), close the modal
+modalCloseEl.onclick = function () {
+  modalEl.style.display = "none";
+  document.querySelector("body").style.overflowY = "scroll";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modalEl) {
+    modalEl.style.display = "none";
+    document.querySelector("body").style.overflowY = "scroll";
+  }
+};
