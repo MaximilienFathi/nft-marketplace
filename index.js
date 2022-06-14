da221639fec439e3683cdf3b381f4c7704d5f336
Maximilien Fathi <maximilian.fathi@gmail.com>
2022-06-13 23:05:25 -0400

    Update left side of creator modal window

--- index.js
+++ index.js
@@ -186,3 +186,99 @@ window.onload = function () {
     // }
   }, 1000);
 };
+
+/////////////////////////////////////////////////////////////
+
+const area_chart_data = {
+  labels: ["M", "T", "W", "T", "F", "S", "S"],
+  datasets: [
+    {
+      label: "apples",
+      data: [12, 19, 3, 17, 6, 3, 7],
+      backgroundColor: "rgba(153,255,51,0.6)",
+      fill: true,
+      borderWidth: 1, // Specify bar border width
+      borderColor: "silver", // Add custom color border (Line)
+    },
+    {
+      label: "oranges",
+      data: [2, 29, 5, 5, 2, 3, 10],
+      backgroundColor: "rgba(255,153,0,0.6)",
+      fill: true,
+      borderWidth: 1, // Specify bar border width
+      borderColor: "silver", // Add custom color border (Line)
+    },
+  ],
+};
+
+const area_ctx = document.querySelector("#creator-area-chart").getContext("2d");
+const myAreaChart = new Chart(area_ctx, {
+  type: "line",
+  data: area_chart_data,
+  options: {
+    responsive: true, // Instruct chart js to respond nicely.
+    maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height
+    // animations: {
+    //   tension: {
+    //     duration: 1000,
+    //     easing: "linear",
+    //     from: 0.5,
+    //     to: 0,
+    //     loop: true,
+    //   },
+    // },
+  },
+});
+
+/////////////////////////////////////////////////////////////
+
+const radar_chart_data = {
+  labels: [
+    "Eating",
+    "Drinking",
+    "Sleeping",
+    "Designing",
+    "Coding",
+    "Cycling",
+    "Running",
+  ],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [65, 59, 90, 81, 56, 55, 40],
+      fill: true,
+      backgroundColor: "rgba(255, 99, 132, 0.2)",
+      borderColor: "rgb(255, 99, 132)",
+      pointBackgroundColor: "rgb(255, 99, 132)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgb(255, 99, 132)",
+    },
+    {
+      label: "My Second Dataset",
+      data: [28, 48, 40, 19, 96, 27, 100],
+      fill: true,
+      backgroundColor: "rgba(54, 162, 235, 0.2)",
+      borderColor: "rgb(54, 162, 235)",
+      pointBackgroundColor: "rgb(54, 162, 235)",
+      pointBorderColor: "#fff",
+      pointHoverBackgroundColor: "#fff",
+      pointHoverBorderColor: "rgb(54, 162, 235)",
+    },
+  ],
+};
+
+const radar_ctx = document
+  .querySelector("#creator-radar-chart")
+  .getContext("2d");
+const myRadarChart = new Chart(radar_ctx, {
+  type: "radar",
+  data: radar_chart_data,
+  options: {
+    elements: {
+      line: {
+        borderWidth: 3,
+      },
+    },
+  },
+});
